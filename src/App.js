4c8aab55e3701b7f57b7fa6c4cbbfadc3fd57f8e
Hunter Carrico <huntercarrico@Hunters-MacBook-Air.local>
2018-11-06 16:57:35 -0700

    minor fix

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import routes from './routes/routes1';
 import axios from 'axios';
 import './App.css';
 
-const PAYMENT_SERVER_URL = '3RD_PARTY_SERVER';
 
 class App extends Component {
   constructor(){
@@ -13,28 +12,7 @@ class App extends Component {
 
     }
   }
-    // onToken = (token) => {
-    //     fetch('/save-stripe-token', {
-    //       method: 'POST',
-    //       body: JSON.stringify(token),
-    //     }).then(response => {
-    //       response.json().then(data => {
-    //         alert(`We are in business, ${data.email}`);
-    //       });
-    //     })
-    //   };
-    
-    onToken = (token) => {
-      axios.post(PAYMENT_SERVER_URL,
-          {
-            source: token.id,
-            currency: 'USD',
-            amount: '10000'
-          })
-          .then(successPayment)
-          .catch(errorPayment);
-    }
-  
+
   render() {
     return (
       <div className="app">
